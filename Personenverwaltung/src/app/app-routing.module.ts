7c054bebff1d3d6f1d9b6540cf70c943fc9d2a68
awab <awab.alshaikhly@edu.tbz.ch>
2021-07-11 00:13:13 +0200

    added the nav bar tabs and the content of the pages

--- Personenverwaltung/src/app/app-routing.module.ts
+++ Personenverwaltung/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
 import { LoginComponent } from './login/login.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { UeberunsComponent } from './ueberuns/ueberuns.component';
@@ -7,7 +9,9 @@ import { UeberunsComponent } from './ueberuns/ueberuns.component';
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'menu', component: MainMenuComponent},
-  {path: 'menu/ueberuns', component: UeberunsComponent}
+  {path: 'menu/ueberuns', component: UeberunsComponent},
+  {path: 'menu/kontakt', component: KontaktComponent},
+  {path: 'menu/info', component: InfoComponent}
 ];
 
 @NgModule({
--- Personenverwaltung/src/app/app.module.ts
+++ Personenverwaltung/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { UeberunsComponent } from './ueberuns/ueberuns.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     routingComponents,
-    UeberunsComponent
+    UeberunsComponent,
+    KontaktComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
